1a8126a38656be2b7cb467f9af305a9534ac027e
villagrachris <villagrachris@gmail.com>
2021-07-14 20:05:07 -0300

    ok

---
+++ src/components/apps1.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App1() {
+
+   return( 
+     
+       <div>
+         <h2>Segundo componente</h2>
+            <p>Usar <strong>Diferentes TAGs de HTML</strong> en este componente</p>
+            <p> Informacion adicional de JavaScript</p>                                     
+  </div>
+      
+   );
+
+}
+
+export default App1;
