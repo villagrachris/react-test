7fd337b618f1ba8298d1bbe8b7056b8fa77a61f9
villagrachris <villagrachris@gmail.com>
2021-07-17 21:23:39 -0300

    ok

--- src/components/apps2.jsx
+++ src/components/apps2.jsx
@@ -1,12 +1,31 @@
 
 import React from 'react';
 
+
+const divStyle = {
+    color: 'blue',
+    font: 'italic',
+    fontsize: '40',
+
+  };
+
+
+
+
 function App2() {
 
     return( 
-        <div style={{ height: 30 }}>
-        <center>Hello World!</center>
-        </div>
+        //<div style={{ height: 30 }}>
+        //<center>Hello World!</center>
+        //</div>
+        
+        <div style={divStyle}>
+            
+            <p><centre>Utilizar estilos en el segundo componente</centre></p>
+            
+        </div> 
+         
+
     );
 }
 
